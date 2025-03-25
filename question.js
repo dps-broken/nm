1dfb7fd316b8af34af9f2f7c9dbb40960e3bce4e
dps-broken <107934635+dps-broken@users.noreply.github.com>
2025-03-25 22:45:44 +0530

    updt

---
+++ question.js
@@ -0,0 +1,119 @@
+// यह रहे 50 रिवीजन के प्रश्न जो तुम्हारे द्वारा पढ़े गए टॉपिक्स से ही हैं:
+
+// 1-10: Prompt & Input Handling
+// prompt() function का उपयोग किसलिए किया जाता है?
+
+// prompt() से मिले डेटा का डिफ़ॉल्ट टाइप क्या होता है?
+
+// parseInt() का उपयोग कब और क्यों किया जाता है?
+
+// नीचे दिए गए कोड का आउटपुट क्या होगा?
+
+
+// let num = prompt("Enter a number");
+// console.log(typeof num);
+// दो संख्याओं को जोड़ने के लिए prompt() से मिले इनपुट को कैसे कन्वर्ट करोगे?
+
+// console.log("Your Name is", Uname); में Uname क्या दर्शाता है?
+
+// parseFloat() और parseInt() में क्या अंतर है?
+
+// Number() और parseInt() में क्या अंतर है?
+
+// नीचे दिए गए कोड का आउटपुट क्या होगा?
+
+
+// let number = "42";
+// console.log(parseInt(number) + 10);
+// "10" + 5 और 10 + "5" का आउटपुट क्या होगा?
+
+// 11-20: Comparison Operators
+// == और === में क्या अंतर है?
+
+// != और !== का क्या अंतर है?
+
+// console.log(5 > 3); का आउटपुट क्या होगा?
+
+// console.log(10 >= "10"); का आउटपुट क्या होगा?
+
+// console.log("5" == 5); और console.log("5" === 5); में क्या फर्क है?
+
+// console.log(0 == false); और console.log(0 === false); में क्या फर्क है?
+
+// console.log(null == undefined); और console.log(null === undefined); में क्या फर्क है?
+
+// console.log("Apple" > "Banana"); का आउटपुट क्या होगा?
+
+// console.log("2" > 1); का आउटपुट क्या होगा?
+
+// नीचे दिए गए कोड का आउटपुट बताओ:
+
+
+// let x = 15, y = "15";
+// console.log(x != y);
+// console.log(x !== y);
+// 21-30: Increment & Decrement Operators
+// ++a और a++ में क्या अंतर है?
+
+// --a और a-- में क्या अंतर है?
+
+// let a = 10; console.log(++a); का आउटपुट क्या होगा?
+
+// let a = 10; console.log(a++); console.log(a); का आउटपुट क्या होगा?
+
+// let b = 5; console.log(--b); का आउटपुट क्या होगा?
+
+// let c = 7; console.log(c--); console.log(c); का आउटपुट क्या होगा?
+
+// let d = 20; let e = ++d + d--; का आउटपुट बताओ।
+
+// console.log(5 + ++a); में पहले ++a execute होगा या 5 +?
+
+// console.log(10 - --b); का आउटपुट क्या होगा अगर b = 5;?
+
+// let x = 5; console.log(x++ + --x + x--); का आउटपुट बताओ।
+
+// 31-40: Assignment Operators
+// += और -= ऑपरेटर का उपयोग कब करते हैं?
+
+// num *= 2; और num = num * 2; क्या एक ही चीज़ है?
+
+// console.log(10 += 5); में क्या ग़लती है?
+
+// num /= 2; और num = num / 2; का क्या अर्थ है?
+
+// let a = 10; a += 5; console.log(a); का आउटपुट क्या होगा?
+
+// let x = 30; x -= 10; console.log(x); का आउटपुट क्या होगा?
+
+// let y = 8; y *= 3; console.log(y); का आउटपुट बताओ।
+
+// let z = 24; z /= 4; console.log(z); का आउटपुट क्या होगा?
+
+// console.log(5 ** 2); का आउटपुट क्या होगा?
+
+// let a = 2; a **= 3; के बाद console.log(a); का आउटपुट बताओ।
+
+// 41-50: String Manipulation & Console Log
+// console.log("Hello " + "World"); का आउटपुट क्या होगा?
+
+// Template literals (Hello ${name}) और string concatenation ("Hello " + name) में क्या अंतर है?
+
+// console.log("Good" + "Morning" + "Pranjal"); का आउटपुट बताओ।
+
+// console.log("Good" , "Morning" , "Pranjal"); और console.log("Good" + "Morning" + "Pranjal"); में क्या फर्क है?
+
+// console.log("Hello" + 5 + 3); और console.log(5 + 3 + "Hello"); का आउटपुट क्या होगा?
+
+// नीचे दिए गए कोड का आउटपुट बताओ:
+
+
+// let name = "Pranjal";
+// console.log(`Hello ${name}, how are you?`);
+// console.log("5" - "2"); का आउटपुट क्या होगा?
+
+// console.log("5" + "2"); का आउटपुट क्या होगा?
+
+// console.log("5" * "2"); और console.log("5" / "2"); का आउटपुट क्या होगा?
+
+// console.log("Hello " + (2 + 3)); और console.log((2 + 3) + " Hello"); में क्या फर्क है?
