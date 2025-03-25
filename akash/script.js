e26b344128b5ec06fd07f5cf8e97e38c98db8374
dps-broken <107934635+dps-broken@users.noreply.github.com>
2025-03-25 22:37:32 +0530

    25 March

---
+++ akash/script.js
@@ -0,0 +1,87 @@
+// let Uname = prompt("Enter Your Name")
+// console.log("Your Name is",Uname);
+
+// let numberOne = prompt('Enter First Number')
+// let numberTwo = prompt('Enter Second Number')
+
+// let result = parseInt(numberOne) + parseInt(numberTwo)
+// console.log("The Sum of Given Number is : ",result);
+
+
+// let numberOne = prompt('Enter First Number')
+// let numberTwo = prompt('Enter Second Number')
+
+// numberOne = parseInt(numberOne)
+// numberTwo = parseInt(numberTwo)
+
+// console.log(numberOne == numberTwo);
+
+// Increment Operator
+
+// let a = 11;
+
+// console.log(++a);
+// console.log(a);
+
+// ===
+// Decrement Operator
+
+// let a = 11;
+
+// console.log(--a);
+// console.log(a);
+
+// comparession opprator
+
+// >
+// <
+// >=
+// <=
+// ==
+// !=
+// ===
+
+// let comA = 20
+// let comB = 80
+
+// console.log(comA == comB);
+// console.log(comA >= comB);
+// console.log(comA <= comB);
+// console.log(comA != comB);
+
+
+// Assignment Operator
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+// let num = 20
+// num += 1
+// num = num + 1
+// console.log(num);
+
+// let num = 27
+// num *= 2
+// num = num * 2
+// console.log(num);
+
+
+// console.log("Hello");
+// let a = "Babu Bhaiya"
+// let b = "Bhosdika"
+// console.log(a);
+// console.log(b,b,"Good Morning",a);
+
+
+// let a = "Good"
+// let b = "Morning"
+// let c = "Afternoon"
+// let d = "Evening"
+
+// let naam = "Pranjal"
+
+// console.log(`hello ${naam} bhaiya`);
+// console.log(a,b,naam);
